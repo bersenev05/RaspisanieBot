21558278b6be271d5784f04beaa51333472cb976
bersenev05 <107613209+bersenev05@users.noreply.github.com>
2023-09-07 17:32:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import asyncio
+import aiogram
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ContentType
 from user_base_worker import user_file
